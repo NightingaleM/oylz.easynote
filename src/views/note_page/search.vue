57aaddb046fa40dfb6d313639ba0496ce0137abf
oy_runchi <oy_runchi@qq.com>
2019-06-20 14:32:32 +0800

    feat: 添加搜索标签

--- src/views/note_page/search.vue
+++ src/views/note_page/search.vue
@@ -1,58 +1,43 @@
 <template>
   <div id="search-page">
-    <span class="is-self-btn" @click="updateCheckSelf">看自己的！{{selectInfo.checkSelf?'': '还有别人的！'}}</span>
-    <v-expansion-panel v-model="panel" class="search-expansiton-box">
-      <v-expansion-panel-content class="expansion-header">
-        <div slot="header">搜索内容：{{selectInfo.originKeyword.replace(/ +/g,',')}}</div>
-        <v-card>
-          <v-card-text>
-            <v-text-field
-              v-model.trim="selectInfo.originKeyword"
-              :counter="40"
-              label="search your note"
-              required
-            ></v-text-field>
-          </v-card-text>
-        </v-card>
-      </v-expansion-panel-content>
-      <v-expansion-panel-content class="expansion-header">
-        <div
-          slot="header"
-        >标签：{{selectInfo.tags.lenght ? '' : selectInfo.tags.map(i=>{return i.tag})}}</div>
-        <v-card>
-          <v-card-text>
-            <v-autocomplete
-              v-model="selectInfo.tags"
-              :items="tagOptions"
-              label="tags"
-              item-text="tag"
-              return-object
-              multiple
-            >
-              <template slot="selection" slot-scope="data">
-                <v-chip
-                  :selected="data.selected"
-                  small
-                  close
-                  class="chip--select-multi"
-                  @input="removeTag(data.item)"
-                >{{ data.item.tag }}</v-chip>
-              </template>
-              <template slot="item" slot-scope="data">
-                <template v-if="typeof data.item !== 'object'">
-                  <v-list-tile-content v-text="data.item"></v-list-tile-content>
-                </template>
-                <template v-else>
-                  <v-list-tile-content>
-                    <v-list-tile-title v-html="data.item.tag"></v-list-tile-title>
-                  </v-list-tile-content>
-                </template>
-              </template>
-            </v-autocomplete>
-          </v-card-text>
-        </v-card>
-      </v-expansion-panel-content>
-    </v-expansion-panel>
+    <v-progress-linear height="2" class="loading-linear" :indeterminate="loading"></v-progress-linear>
+    <div class="select-box">
+      <v-text-field
+        v-model.trim="selectInfo.originKeyword"
+        :counter="40"
+        label="search your note"
+        required
+      ></v-text-field>
+      <v-autocomplete
+        v-model="selectInfo.tags"
+        :items="tagOptions"
+        label="tags"
+        item-text="tag"
+        return-object
+        multiple
+      >
+        <template slot="selection" slot-scope="data">
+          <v-chip
+            :selected="data.selected"
+            small
+            close
+            class="chip--select-multi"
+            @input="removeTag(data.item)"
+          >{{ data.item.tag }}</v-chip>
+        </template>
+        <template slot="item" slot-scope="data">
+          <template v-if="typeof data.item !== 'object'">
+            <v-list-tile-content v-text="data.item"></v-list-tile-content>
+          </template>
+          <template v-else>
+            <v-list-tile-content>
+              <v-list-tile-title v-html="data.item.tag"></v-list-tile-title>
+            </v-list-tile-content>
+          </template>
+        </template>
+      </v-autocomplete>
+      <span class="is-self-btn" @click="updateCheckSelf">看自己的！{{selectInfo.checkSelf?'': '还有别人的！'}}</span>
+    </div>
     <div id="note-content-box" v-scroll:#note-content-box="onScroll">
       <v-list two-line v-show="noteList.length" id="scroll-box">
         <template v-for="(item) in noteList">
@@ -85,8 +70,7 @@ export default {
       // originKeyword: ""
       timeOut: null,
       addPageLock: false,
-      panel: null, // 控制扩展面板的展开、关闭
-      panelTimeOut: null // 到事件自动闭合所有扩展面板
+      loading: false
     };
   },
   methods: {
@@ -125,28 +109,17 @@ export default {
     ...mapGetters(["keywords"])
   },
   watch: {
-    panel: {
-      handler(status) {
-        if (status === null) return;
-        clearTimeout(this.panelTimeOut);
-        this.panelTimeOut = setTimeout(() => {
-          this.panel = null;
-        }, 4000);
-      }
-    },
     selectInfo: {
       handler(selectInfo) {
         if (selectInfo.tags.length > 5) {
           selectInfo.tags = selectInfo.tags.splice(0, 5);
           this.awakeSnackbar({ text: "标签数不可超过5！", timeout: 2000 });
         }
-        clearTimeout(this.panelTimeOut);
-        this.panelTimeOut = setTimeout(() => {
-          this.panel = null;
-        }, 4000);
         clearTimeout(this.timeOut);
         this.timeOut = setTimeout(async () => {
-          let res = await this.getSearchNote({ init: true });
+          this.loading = true;
+          await this.getSearchNote({ init: true });
+          this.loading = false;
           this.addPageLock = false;
         }, 600);
       },
@@ -158,35 +131,20 @@ export default {
 
 <style lang="less" scoped>
 #search-page {
-  margin-top: 75px;
+  .loading-linear {
+    margin: 0;
+    position: absolute;
+    top: 35px;
+    z-index: 501;
+  }
   .is-self-btn {
-    position: fixed;
-    top: 110px;
-    right: 5px;
+    position: absolute;
     z-index: 499;
     font-size: 12px;
     border: 1px solid #b3b3b3;
     color: #939393;
     border-radius: 3px;
     padding: 0 3px;
-  }
-  .search-expansiton-box {
-    padding: 0 20px;
-    background-color: #fff;
-    position: fixed;
-    top: 35px;
-    left: 0;
-    z-index: 500;
-    width: 100%;
-    .expansion-header {
-      .v-expansion-panel__header {
-        padding: 0 24px;
-        min-height: 30px;
-      }
-    }
-    .v-card__text {
-      padding: 0 10px;
-    }
   }
   .note-li {
     // padding: 0 20px;
